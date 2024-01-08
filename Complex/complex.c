5e8a3c7824c680e8641c6ebc1c123daa37101335
Madhan <madhansaikrishna@outlook.com>
2024-01-09 02:43:46 +0530

    Updated the Complex operations functions

--- Complex/complex.c
+++ Complex/complex.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "complex.h"
 #include <math.h>
+#include <stdlib.h>
 
 Complex Cxcreate(double a, double b)
 {
@@ -16,5 +17,58 @@ Complex Cxaddition(Complex c1, Complex c2)
     C.real = c1.real+ c2.real;
     C.imag = c1.imag + c2.imag;
     return C;
+}
+
+Complex Cxmultiplication(Complex c1, Complex c2)
+{
+    Complex C;
+    C.real = (c1.real*c2.real) - (c1.imag*c2.imag);
+    C.imag = (c1.imag*c2.real) + (c1.real*c2.imag);
+    return C;
+}
+
+Complex Cxdivision(const Complex c1, const Complex c2)
+{
+    Complex C;
+    C = Cxmultiplication(c1, Cxinverse(c2));
+    return C;
+}
+
+Complex Cxconjugate(Complex C)
+{
+    Complex Cconj;
+    Cconj.real = (Cxmod(C)*Cxmod(C))*(Cxinverse(C).real);
+    Cconj.imag = (Cxmod(C)*Cxmod(C))*(Cxinverse(C).imag);
+    return Cconj;
+}
+
+Complex Cxinverse(Complex C)
+{
+    Complex Cinv;
+    Cinv.real = C.real/(Cxmod(C)*Cxmod(C));
+    Cinv.imag = C.imag/(Cxmod(C)*Cxmod(C)*-1);
+    return Cinv;
+}
+
+Complex Cxsqrt(Complex C)
+{
+    Complex Crt;
+    Crt.real = sqrt((Cxmod(C)+C.real)/2.0);
+    Crt.imag = sqrt((Cxmod(C)-C.real)/2.0)*(C.imag/C.real)*(C.imag/abs(C.imag));
+    return Crt;
+}
+
+double Cxarg(Complex C)
+{
+    double arg;
+    arg = atan((C.imag/C.real));
+    return arg;
+}
+
+double Cxmod(Complex C)
+{
+    double mod;
+    mod = sqrt((C.real*C.real) + (C.imag*C.imag));
+    return mod;
 }
 
--- Complex/complex.h
+++ Complex/complex.h
@@ -7,8 +7,9 @@ typedef struct stcomplex
 Complex Cxcreate(double , double);
 Complex Cxaddition(Complex , Complex);
 Complex Cxmultiplication(Complex, Complex);
-Complex Cxdivision(Complex , Complex);
+Complex Cxdivision(const Complex , const Complex);
 Complex Cxconjugate(Complex);
 Complex Cxsqrt(Complex);
+Complex Cxinverse(Complex);
 double Cxarg(Complex);
 double Cxmod(const Complex);
